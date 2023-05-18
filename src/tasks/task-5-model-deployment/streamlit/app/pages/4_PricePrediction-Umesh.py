939eb826182a5757837105f7b411b81ea4cfe874
Umesh Patil <umesh.patil@gmail.com>
2023-05-18 10:22:09 +0530

    streamlit update (PricePrediction_Umesh) - input from user

--- src/tasks/task-5-model-deployment/streamlit/app/pages/4_PricePrediction-Umesh.py
+++ src/tasks/task-5-model-deployment/streamlit/app/pages/4_PricePrediction-Umesh.py
@@ -1,10 +1,41 @@
 import streamlit as st
 import subprocess
+import csv
 
-st.subheader("Generate property price")
+st.subheader("Predict price for an apartment")
 
+area = st.number_input('Area in Sq. ft.', min_value = 10)
+# st.write('Area: ', area)
+
+num_bath_rooms = st.number_input('Number of bathrooms')
+# st.write('Bathrooms: ', num_bath_rooms)
+
+num_bed_rooms = st.number_input('Number of bedrooms')
+# st.write('Bedrooms: ', num_bed_rooms)
+
+zone = st.selectbox(
+    'Which zone would you prefer?',
+    ('Mirpur', 'Chattogram City', 'Bashundhara R/A', 'Khilgaon', 'Mohammadpur', 'Uttara', 
+     'Badda', 'Dakshin Khan', 'Sub-district of Chattogram', 'Dhaka Cantonment'))
+# st.write('You selected:', zone)
+
+purpose = st.selectbox(
+    'Sale or rent?',
+    ('sale', 'rent'))
+# st.write('You selected:', purpose)
+
+f = open('input.csv', 'w') #write mode 
+writer = csv.writer(f, delimiter=',', quotechar='"')
+writer.writerow(["area","num_bath_rooms","num_bed_rooms","zone","purpose"])
+writer.writerow([area, num_bath_rooms, num_bed_rooms, zone, purpose])
+f.close()
+
+# Sent the input to the R function
 process = subprocess.Popen(["Rscript", "predict_property_price.R"], 
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
+# Read, format and print the prediction
 result = process.communicate()
-st.write(result)
+predicted_price = list(result)[0].split(' ')[2]
+st.write(f'Price prediction: {predicted_price} BDT for the {purpose} of {area} Sq. ft apartment in {zone}')
+
--- src/tasks/task-5-model-deployment/streamlit/predict_property_price.R
+++ src/tasks/task-5-model-deployment/streamlit/predict_property_price.R
@@ -8,54 +8,50 @@ predict_property_price <- function(area = 1000,
                                    num_bath_rooms = 1, 
                                    num_bed_rooms = 2, 
                                    zone='Mirpur', 
-                                   purpose = "sale"){    
-
+                                   purpose = "sale",
+                                   building_nature = "Residential"
+){    
+  #     Load the model
+  if(purpose == "sale"){
     #     Create a data frame with user input
     newdata <- data.frame(area, num_bath_rooms, num_bed_rooms, zone)
-
-    #     Load the model
-    if(purpose == "sale")
-        mod.reg <- readRDS("lmer.sale.allZones.rds")
-    else if(purpose == "rent")
-        mod.reg <- readRDS("lmer.rent.allZones.rds")
-    else{
-        print("ERROR: Purpose should be either 'sale' or 'rent'")
-        return(0)
-    }
-
-    #     Generate predictions
-    y_predicted <- predict(mod.reg, newdata = newdata, re.form = NULL)
-
-    #     Back-transform the log-transformed price value
-    y_predicted_backtransformed <- exp(y_predicted)
-    
-    # print(paste(round(as.numeric(y_predicted_backtransformed)), "BDT", "for", purpose, "of", area, "Sq. ft", "in", zone))
-    return(round(as.numeric(y_predicted_backtransformed)))
+    mod.reg <- readRDS("lmer.sale.allZones.rds")
+  }
+  
+  else if(purpose == "rent"){
+    #     Create a data frame with user input
+    newdata <- data.frame(area, num_bath_rooms, num_bed_rooms, zone, building_nature)
+    mod.reg <- readRDS("lmer.rent.allZones.rds")
+  }
+  
+  else{
+    print("ERROR: Purpose should be either 'sale' or 'rent'")
+    return(0)
+  }
+  
+  #     newdata <- data.frame(area, num_bath_rooms, num_bed_rooms, zone, building_nature, purpose)
+  # #     mod.reg <- readRDS(model)
+  #     mod.reg <- readRDS("lmer.comb.allZones.rds")
+  
+  #     Generate predictions
+  y_predicted <- predict(mod.reg, newdata = newdata, re.form = NULL)
+  
+  #     Back-transform the log-transformed price value
+  y_predicted_backtransformed <- exp(y_predicted)
+  
+  # print(paste(round(as.numeric(y_predicted_backtransformed)), "BDT", "for", purpose, "of", area, "Sq. ft", "in", zone))
+  return(round(as.numeric(y_predicted_backtransformed)))
 }
 
-# Code to call the function based on the input
-# A list of top 10 zones in terms of the no. data points in the training set
-zones_top_10 = c('Mirpur',
- 'Chattogram City',
- 'Bashundhara R/A',
- 'Khilgaon',
- 'Mohammadpur',
- 'Uttara',
- 'Badda',
- 'Dakshin Khan',
- 'Sub-district of Chattogram',
- 'Dhaka Cantonment')
 
-area           = 4000
-num_bath_rooms = 1
-num_bed_rooms  = 2
-zone           = zones_top_10[3]
-purpose        = "sale"
+input = read.csv(file = 'input.csv')
+
+area           = input$area[1]
+num_bath_rooms = input$num_bath_rooms[1]
+num_bed_rooms  = input$num_bed_rooms[1]
+zone           = input$zone[1]
+purpose        = input$purpose[1]
 
 predicted_price = predict_property_price(area, num_bath_rooms, num_bed_rooms, zone, purpose)
 
-print(paste(predicted_price, "BDT", "for", purpose, "of", area, "Sq. ft", "in", zone))
-
-# print("")
-# print(paste("Predicted sale price:", predicted_price))
-
+print(paste('', predicted_price, ''))
